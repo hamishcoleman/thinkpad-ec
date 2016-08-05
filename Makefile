hamishcoleman/thinkpad-ec

commit 48b019b6603f3563cdf633ac99ceafebfce0645f

Update the base image of the t430 patch to use the latest lenovo release (no EC changes in this version)

Makefile
@@ -47,7 +47,7 @@ patched.t430.iso: g1uj38us.iso
 patched.t430.257.iso: g1uj25us.iso
 	$(call patched_iso,$<,$@)
 
-patched.t430s.iso: g7uj18us.iso
+patched.t430s.iso: g7uj19us.iso
 	$(call patched_iso,$<,$@)
 
 patched.t530.iso: g4uj30us.iso
@@ -276,5 +276,6 @@ $(call rule_iso,x230.G2HT35WW.s01D3000.FL2,g2uj23us.iso)
 $(call rule_iso,t530.G4HT39WW.s01D5100.FL2,g4uj30us.iso)
 $(call rule_iso,w530.G4HT39WW.s01D5200.FL2,g5uj28us.iso)
 $(call rule_iso,t430s.G7HT39WW.s01D8000.FL2,g7uj18us.iso)
+$(call rule_iso,t430s.G7HT39WW.s01D8000.FL2,g7uj19us.iso)
 $(call rule_iso,x230t.GCHT25WW.s01DA000.FL2,gcuj24us.iso)
 

g7uj19us.iso.orig.desc
@@ -1,0 +1,1 @@
+t430s

g7uj19us.iso.orig.sha1
@@ -1,0 +1,1 @@
+be900c38e185831ae5fa93266a806bb394ba7ac7  g7uj19us.iso.orig

t430s.G7HT39WW.s01D8000.FL2.slice
@@ -1,1 +1,1 @@
-g7uj18us.iso.orig 0x2c5600 16912384
+g7uj19us.iso.orig 0x375e00 16912384

commit 01ad8a01f6a0d885f5992018147caa5ce223dd0e

BUG: fix incorrectly updated line (see #9)
This fixes an issue introduced with 48b019b6603f3563cdf633ac99ceafebfce0645f

Makefile
@@ -276,7 +276,6 @@ $(call rule_iso,t430.G1HT34WW.s01D2000.FL2,g1uj25us.iso)
 $(call rule_iso,x230.G2HT35WW.s01D3000.FL2,g2uj23us.iso)
 $(call rule_iso,t530.G4HT39WW.s01D5100.FL2,g4uj30us.iso)
 $(call rule_iso,w530.G4HT39WW.s01D5200.FL2,g5uj28us.iso)
-$(call rule_iso,t430s.G7HT39WW.s01D8000.FL2,g7uj18us.iso)
 $(call rule_iso,t430s.G7HT39WW.s01D8000.FL2,g7uj19us.iso)
 $(call rule_iso,x230t.GCHT25WW.s01DA000.FL2,gcuj24us.iso)
 